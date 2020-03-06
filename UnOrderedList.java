20b30795e3da616ceed0d92251e632ea8c3a319f
prathamesh <prathmesh6464@gmail.com>
2020-03-06 17:19:06 +0530

    ADDED : Empty list feature added.

--- UnOrderedList.java
+++ UnOrderedList.java
@@ -1,24 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.File;
-
-
-//CREATING NODE FOR LINK LIST
 class Node
 {
 	String word;
-	Node next = null;
-} 
+	Node next=null;
+}
 
 
-//CREATING LINK LIST CLASS
-class LinkList<T>
+class LinkList
 {
+<<<<<<< Updated upstream
 	Node head = new Node();
 	public void insertWord(String word)
 	{
@@ -126,80 +115,22 @@ class LinkList<T>
 
 	//METHOD OF TAKING INPUT
 	String takeInput()
+=======
+	Node emptyList()
+>>>>>>> Stashed changes
 	{
-		System.out.println("Enter the word which you want to find and delete : ");
-		Scanner scannerObject = new Scanner(System.in);
-		String findWord = scannerObject.next();
-		return findWord;		
-	}
-
-
-	//METHOD OF SAVING IN SAME FILE
-	void saveIntoSameFile() throws IOException
-	{
-		FileWriter fileWriterObject  = new FileWriter("//home//admin1//Documents//GamblerProblem//SnakeAndLadder//TempFile.txt",true);
-		BufferedWriter bufferWriterObject = new BufferedWriter(fileWriterObject);
-
-
-		try 
-		{
-			Node tempHead = head;
-			while(tempHead.next != null)
-			{
-				tempHead = tempHead.next;
-				bufferWriterObject.newLine();
-				System.out.println("Writing word data to file : "+tempHead.word);
-				bufferWriterObject.write(tempHead.word);
-			}
-		}
-		catch(NullPointerException e)
-		{
-			System.out.println("File write operation completed.");
-		}
-		bufferWriterObject.flush();
-		bufferWriterObject.close();		
+		Node head = new Node();
+		return head.next;
 	}
 }
 
 
-//UNORDERED LIST CLASS
-public class UnOrderedList
-{	
-	//MAIN METHOD
-	public static void main(String[] args) throws IOException,NullPointerException,FileNotFoundException,IOException
+class UnOrderedList
+{
+	public static void main(String[] args)
 	{
-		FileReader fileRead = new FileReader(new File("//home//admin1//Documents//GamblerProblem//SnakeAndLadder//TempFile.txt"));
-		BufferedReader bufferFileRead = new BufferedReader(fileRead);
-		String line = bufferFileRead.readLine();
-		LinkList<String> wordData = new LinkList<String>();
-
-
-		while(line != null) 
-		{
-			try
-			{
-				String[] splitWords = line.split(" ");
-				for(String word : splitWords)
-				{
-					wordData.insertWord(word);
-				}
-				line = bufferFileRead.readLine();
-			}
-			catch(NullPointerException e)
-			{
-				System.out.println("List endded");
-			}
-		}
-		bufferFileRead.close();
-
-		
-		wordData.showWordList();	
-		String wordToDelete=wordData.takeInput();	
-		wordData.deleteWord(wordToDelete);	
-		wordData.saveIntoSameFile();
-		wordData.showWordList();
-		wordData.isWordPresent(wordToDelete);	
-		wordData.saveIntoSameFile();
-		wordData.showWordList();
+		LinkList linkListFeatures = new LinkList();
+		Node returnedEmptyResult = linkListFeatures.emptyList();
+		System.out.println("Empty list : "+returnedEmptyResult);
 	}
 }
