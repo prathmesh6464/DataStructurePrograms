e7babfd83f7e2ff219acad28998343fea88ab722
Prathamaesh <prathmesh6464@gmail.com>
2020-03-08 19:14:42 +0530

    REFACTORED : Added enqueue feature.

--- BankingCashCounter/BankingCashCounter.java
+++ BankingCashCounter/BankingCashCounter.java
@@ -1,7 +1,7 @@
 package BankingCashCounter;
 
 
-//PERSON NODE
+//PERSON Person
 class Person
 {
 	int balanceCash = 100;
@@ -10,15 +10,43 @@ class Person
 
 
 //QUEUE CLASS
-class Queue
+class Queue<T>
 {
 	Person startRow = new Person();
-	
-	
+	int size=0;
+
+
 	//EMPTY QUEUE METHOD
 	Person emptyQueue()
 	{
 		return startRow;
+	}
+
+
+	//ENQUEUE METHOD
+	public void enQueue(int balanceCash)
+	{
+		//VARIABLES OF PERSON
+		Person nextPersonInQueue = new Person();
+		nextPersonInQueue.balanceCash = balanceCash;
+		nextPersonInQueue.nextPerson = null;
+
+
+		if(startRow.nextPerson == null )
+		{
+			startRow.nextPerson = nextPersonInQueue;
+			size++;
+		}
+		else
+		{
+			Person temporaryStartRow = startRow;
+			while(temporaryStartRow.nextPerson != null)
+			{
+				temporaryStartRow = temporaryStartRow.nextPerson;
+			}
+			temporaryStartRow.nextPerson = nextPersonInQueue;
+			size++;
+		}
 	}
 }
 
@@ -30,9 +58,11 @@ public class BankingCashCounter
 	public static void main(String[] args) 
 	{
 		Queue newPerson = new Queue();
-		
-		
+
+
 		//EMPTY QUEUE METHOD CALLED
 		newPerson.emptyQueue();
+		newPerson.enQueue(100);
+		
 	}
 }
