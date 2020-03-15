05c8462db7b79e83ddcd27caad47aa4aa351af2a
Prathamaesh <prathmesh6464@gmail.com>
2020-03-15 19:26:26 +0530

    ADDED : Added calender class.

---
+++ Calender/Calender.java
@@ -0,0 +1,20 @@
+package Calender;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+//CALENDER CLASS
+public class Calender 
+{
+	//MAIN CLASS
+	public static void main(String[] args)
+	{
+		Date date = new Date();
+		SimpleDateFormat month = new SimpleDateFormat("M");
+		SimpleDateFormat current_year = new SimpleDateFormat("yyyy");
+
+		
+		String[] week = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};		
+	}
+}
+
